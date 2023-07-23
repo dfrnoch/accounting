f4455f2ecbe133cb1ece5e1e8f3b20e3b37d8bf9
lnxcz <frnoch@pm.me>
2023-07-23 13:34:19 +0200

    fix: :bug: fix router

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
-/* @refresh reload */
-import { render } from "solid-js/web";
-
 import "@unocss/reset/tailwind.css";
-import "virtual:uno.css";
+import "uno.css";
+
+import { render } from "solid-js/web";
+import { Router } from "@solidjs/router";
 
 import App from "./App";
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+render(
+  () => (
+    <Router>
+      <App />
+    </Router>
+  ),
+  document.getElementById("root") as HTMLElement,
+);
